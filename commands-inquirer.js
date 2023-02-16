725318d5a44c0731173d345c943c5c5a63ec524b
Edward Tanguay (windows) <edward@tanguay.info>
2023-02-16 22:05:39 +0100

    commands-inq

--- commands-inquirer.js
+++ commands-inquirer.js
@@ -3,6 +3,74 @@ import { Command } from 'commander';
 
 const program = new Command();
 
+const reactQuestions = [
+	{
+		type: 'input',
+		name: 'name',
+		message: chalk.hex('#a08c95').bold('project name:')
+	},
+	{
+		type: 'list',
+		name: 'framework',
+		choices: [
+			chalk.hex('#A7C7E7')('react'),
+			chalk.green('vue'),
+			chalk.hex('#ff7247')('angular')
+		],
+		message: chalk.hex('#a08c95').bold('selected framework: ')
+	},
+	{
+		type: 'list',
+		name: 'menu',
+		choices: listMenu,
+		message: chalk.hex('#a08c95').bold('react template with:  '),
+		when: function (answers) {
+			return answers['framework'] === chalk.hex('#A7C7E7')('react');
+		}
+	},
+	{
+		type: 'list',
+		name: 'hooks',
+		message: chalk.hex('#a08c95').bold('selected hook: '),
+		choices: listHooks,
+		when: function (answers) {
+			return (
+				answers['framework'] === chalk.hex('#A7C7E7')('react') &&
+				answers['menu'] === 'simple page'
+			);
+		}
+	},
+
+	{
+		type: 'list',
+		name: 'backend',
+		choices: ['yes', 'no'],
+		message: chalk.hex('#a08c95').bold('need a backend? '),
+		when: function (answers) {
+			return (
+				(answers['framework'] === chalk.hex('#A7C7E7')('react') &&
+					answers['menu'] === 'simple page(nextjs/usecontext)' &&
+					answers['menu'] === 'simple page') ||
+				answers['menu'] === 'menu(zustand)' ||
+				answers['menu'] === 'menu' ||
+				answers['framework'] === chalk.green('vue') ||
+				answers['framework'] === chalk.hex('#ff7247')('angular')
+			);
+		}
+	},
+	{
+		type: 'input',
+		name: 'connectionString',
+		message: chalk.hex('#a08c95').bold('MongoDB connection string: '),
+		when: function (answers) {
+			return (
+				answers['menu'] === 'member authentication(CRUD)' ||
+				answers['menu'] === 'authentication and mongodb(CRUD API)'
+			);
+		}
+	}
+];
+
 program
 	.command('react')
 	.argument('<directory>')
